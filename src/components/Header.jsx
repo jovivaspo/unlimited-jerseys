30644ee2590b54e657ae93b44f14d9a5a3bd8797
jovivaspo <jovivaspo@gmail.com>
2023-05-29 19:08:05 +0200

    New design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export const Header = () => {
   return (
     <header className="w-full">
         <div className="w-full p-2 bg-yellow flex flex-col justify-center items-center">
-            <p className="text-black  text-center  text-sm font-bold">Descuento en camistas temporada 22-23</p>
+            <p className="text-black  text-center  text-sm font-bold">Descuento en camisetas temporada 22-23</p>
         </div>
         <CarouselHeader items={images}/>
     </header>
