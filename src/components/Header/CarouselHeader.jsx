8171f690050dbd3f859d1c381971cb3c08618e51
jovivaspo <jovivaspo@gmail.com>
2024-02-06 20:12:13 +0100

    Add hero white bg

--- src/components/Header/CarouselHeader.jsx
+++ src/components/Header/CarouselHeader.jsx
@@ -6,7 +6,7 @@ export const CarouselHeader = ({ items }) => {
     <Carousel className='hidden md:block' showIndicators={false} showStatus={false} showThumbs={false} showArrows={true} interval={6000} transitionTime={2000} stopOnHover={false} swipeable={true}>
       {items.map((item, index) => {
         return (
-          <div className='w-full h-[700px] bg-yellow mb-8  sm:h-auto sm:min-h-[280px] md:min-h-[336px] md:mb-24 lg:min-h-[450px] xl:min-h-[568px] 2xl:max-h-[700px]' key={index}>
+          <div className='w-full h-[700px] bg-transparent mb-8  sm:h-auto sm:min-h-[280px] md:min-h-[336px] md:mb-24 lg:min-h-[450px] xl:min-h-[568px] 2xl:max-h-[700px]' key={index}>
             {item.link ? (
               <a className='w-full h-full block' href={item.link} target='_blank' rel='noreferrer'>
                 <img srcSet={`${item.src[1]} 640w, ${item.src[0]}`} sizes='(max-width: 640px) 640px' src={item.src[0]} alt={item.alt} className='w-full h-auto object-cover' />
--- src/components/Header/HeroHeader.jsx
+++ src/components/Header/HeroHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const HeroHeader = ({ item, index }) => {
   return (
-    <div className='block md:hidden w-full h-[700px] bg-yellow mb-8  sm:h-auto sm:min-h-[280px] md:min-h-[336px] md:mb-24 lg:min-h-[450px] xl:min-h-[568px] 2xl:max-h-[700px]' key={index}>
+    <div className='block md:hidden w-full h-[700px] bg-transparent mb-8  sm:h-auto sm:min-h-[280px] md:min-h-[336px] md:mb-24 lg:min-h-[450px] xl:min-h-[568px] 2xl:max-h-[700px]' key={index}>
       {item.link ? (
         <a className='w-full h-full block' href={item.link} target='_blank' rel='noreferrer'>
           <img srcSet={`${item.src[1]} 640w, ${item.src[0]}`} sizes='(max-width: 640px) 640px' src={item.src[0]} alt={item.alt} className='w-full h-auto object-cover' />
